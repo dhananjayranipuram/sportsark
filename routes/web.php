87c8af43a89e4406290e8496b4ce7c1675c80ef5
dhananjaydevmonkey <dhananjaya.k@devmonkey.in>
2025-02-13 14:22:17 +0530

    fix

--- routes/web.php
+++ routes/web.php
@@ -57,4 +57,45 @@ Route::any('/verify-otp-login', [SiteController::class, 'verifyOtpLogin']);
 
 Route::any('/book-ground', [SiteController::class, 'bookGround']);
 
+//Clear Cache facade value:
+Route::get('/clear-cache', function() {
+    $exitCode = Artisan::call('cache:clear');
+    return '<h1>Cache facade value cleared</h1>';
+});
+
+//Reoptimized class loader:
+Route::get('/optimize', function() {
+    $exitCode = Artisan::call('optimize');
+    return '<h1>Reoptimized class loader</h1>';
+});
+
+//Route cache:
+Route::get('/route-cache', function() {
+    $exitCode = Artisan::call('route:cache');
+    return '<h1>Routes cached</h1>';
+});
+
+//Clear Route cache:
+Route::get('/route-clear', function() {
+    $exitCode = Artisan::call('route:clear');
+    return '<h1>Route cache cleared</h1>';
+});
+
+//Clear View cache:
+Route::get('/view-clear', function() {
+    $exitCode = Artisan::call('view:clear');
+    return '<h1>View cache cleared</h1>';
+});
+
+//Clear Config cache:
+Route::get('/config-cache', function() {
+    $exitCode = Artisan::call('config:cache');
+    return '<h1>Clear Config cleared</h1>';
+});
+
+//To create symbolic link
+Route::get('/sym-link', function () {
+    Artisan::call('storage:link');
+});
+
 require __DIR__.'/auth.php';
